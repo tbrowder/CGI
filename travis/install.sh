f1b8eb75af7dd039ab2661e4ea973b81d64f979c
Tom Browder <tom.browder@gmail.com>
2017-09-26 17:31:43 -0500

    correct install script errors

--- travis/install.sh
+++ travis/install.sh
@@ -3,9 +3,8 @@
 apache2ctl stop
 
 # put files in proper place
-#cp ./travis/*.cgi /var/www/html
 cp ./travis/*.cgi /usr/lib/cgi-bin
-#cp ./travis/*.conf /etc/apache2/conf-available
+cp ./travis/*.conf /etc/apache2/conf-available
 
 a2disconf my-cgi
 a2enconf my-cgi
